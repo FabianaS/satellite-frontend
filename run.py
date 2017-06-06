a56a16193aaa82763f0fcaffce129e867bd51f4f
Pedro Guzman <pguzmanb498@ulacit.ed.cr>
2017-06-06 15:26:35 +0000

    Reference added to satellite JS main file

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ import os
 from satellite import app
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 7443))
-    app.run('127.0.0.1', port)
+    port = int(os.environ.get("PORT", 8080))
+    app.run('0.0.0.0', port)
